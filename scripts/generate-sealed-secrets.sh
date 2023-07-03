3609d45a4a69ba8a24958da509fa363b26bd3df5
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-07-03 13:25:31 +0530

    Fixing the generate-sealed-secrets.sh script

--- scripts/generate-sealed-secrets.sh
+++ scripts/generate-sealed-secrets.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-find "./kubernetes/" -type f -name "*.secret.yaml" -print0 | while IFS= read -r -d '' filepath; do
+find "./kubernetes/" -type f \( -name "*.secret.yaml" -o -name "secret.yaml" \) -print0 | while IFS= read -r -d '' filepath; do
+
+  secretFilename=$(basename "$filepath")
+  echo $secretFilename
+
+  if [ "$secretFilename" == "secret.yaml" ]; then
+    sealedSecretFilename="$(dirname $filepath)/sealed-secret.yaml"
+  else
+    sealedSecretFilename="${filepath%.secret.yaml}.sealed-secret.yaml"
+  fi
+
   kubeseal \
-    --cert ./terragrunt/modules/setup-cluster/sealed-secrets/tls.crt \
+    --cert ./terragrunt/modules/setup-cluster/tls.crt \
     --scope cluster-wide -o yaml \
-    <"$filepath" >"${filepath%.secret.yaml}.sealed-secret.yaml"
+    <"$filepath" >"$sealedSecretFilename"
+
 done
