c1244216a828346baee5d55dc8f44e1d39f74f9c
Archisman Mridha <archismanmridha12345@gmail.com>
2023-12-31 17:40:53 +0530

    Creating init containers (which run migrations) for Kafka and Debezium

---
+++ scripts/create-debezium-connectors.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## This script will be executed inside the 'debezium-migrate' container defined in ./compose.yaml.
+
+apk add curl
+
+echo ⏳ Waiting for Debezium to start...
+until curl --output /dev/null --silent --head --fail http://debezium:8083; do
+	echo .
+	sleep 5
+done
+
+echo "🚀 Debezium is up! Creating connectors..."
+
+for file in "/debezium/"*; do
+  if [ -f "$file" ]; then
+		curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://debezium:8083/connectors/ \
+			-d "$(cat "$file")"
+
+		sleep 10
+  fi
+done
+
+echo "✅ Debezium connectors created successfully."
