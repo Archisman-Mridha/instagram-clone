05120a3a14420d732e6f2f634299400dc1792859
Archisman Mridha <archismanmridha12345@gmail.com>
2023-12-31 17:51:24 +0530

    Defining PostgreSQL database schema for profiles microservice

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -10,3 +10,14 @@ CREATE TABLE users (
 );
 CREATE INDEX email_idx_users ON users (email);
 CREATE INDEX username_idx_users ON users (username);
+
+-- Profiles Microservice
+CREATE TABLE profiles (
+  id INT PRIMARY KEY,
+
+  name VARCHAR(25) NOT NULL,
+  username VARCHAR(25) NOT NULL UNIQUE,
+
+  profile_picture_uri VARCHAR(250)
+);
+CREATE INDEX username_idx_profiles ON profiles (username);
