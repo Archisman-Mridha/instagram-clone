5e0662135d26c09db14177f857a9c8d2966a1105
Archisman Mridha <archismanmridha12345@gmail.com>
2023-12-31 17:46:27 +0530

    Writing protobuf definitions for the gRPC API that profiles microservice will expose

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::{env, path::PathBuf};
 fn main( ) {
   let outputDirectory= PathBuf::from(env::var("OUT_DIR").unwrap( ));
 
-  let microservices= vec![ "users" ];
+  let microservices= vec![ "users", "profiles" ];
 
   for microservice in microservices {
     let protoFilePath= format!("backend/protos/{}_microservice.proto", microservice);
