8f0f3d37032383768644f269840598aa5368f130
Archisman Mridha <archismanmridha12345@gmail.com>
2023-12-31 18:08:23 +0530

    Defining PostgreSQL database schema for posts-microservice

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -29,3 +29,13 @@ CREATE TABLE followships (
 );
 CREATE INDEX follower_id_idx_followships ON followships (follower_id, followee_id);
 CREATE INDEX followee_id_idx_followships ON followships (followee_id, follower_id);
+
+-- Posts Microservice
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	owner_id INT NOT NULL,
+
+	description VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT now( )
+);
+CREATE INDEX owner_id_idx_posts ON posts (owner_id, created_at DESC);
